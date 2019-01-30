d3aeb34367bb0458d9c5d7b7b705a261605fb379
Steve Kemp <steve@steve.org.uk>
2019-01-30 19:59:52 +0200

    Added simple token-class

---
+++ token/token.go
@@ -0,0 +1,20 @@
+package token
+
+// TokenType is a string
+type TokenType string
+
+// Token struct represent the lexer token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// pre-defined TokenType
+const (
+	EOF      = "EOF"
+	INT      = "INT"
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	SLASH    = "/"
+)
