027e6ec1a6f4e0b77d6c54eacca401af34327c53
Steve Kemp <steve@steve.org.uk>
2019-02-04 19:54:59 +0200

    Prepare for the future!  Allow sin/cos to be parsed.
    These are not yet used.

--- token/token.go
+++ token/token.go
@@ -11,13 +11,37 @@ type Token struct {
 
 // pre-defined TokenType
 const (
-	EOF      = "EOF"
-	ERROR    = "ERROR"
-	NUMBER   = "NUMBER"
+	EOF    = "EOF"
+	ERROR  = "ERROR"
+	NUMBER = "NUMBER"
+	IDENT  = "IDENT"
+
+	// simple operations
 	PLUS     = "+"
 	MINUS    = "-"
 	ASTERISK = "*"
 	SLASH    = "/"
-	MOD      = "%"
-	POWER    = "^"
+
+	// advanced operations
+	MOD   = "%" // todo
+	POWER = "^" // todo
+
+	// complex operations
+	COS = "cos"
+	SIN = "sin"
+	// TAN = "tan" // todo / impossible?
 )
+
+// reversed keywords
+var keywords = map[string]TokenType{
+	"cos": COS,
+	"sin": SIN,
+}
+
+// LookupIdentifier used to determinate whether identifier is keyword nor not
+func LookupIdentifier(identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+	return ERROR
+}
