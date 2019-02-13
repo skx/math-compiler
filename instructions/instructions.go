29e19c7bde4b0d98bd197f86583a2b839a3a1b6c
Steve Kemp <steve@steve.org.uk>
2019-02-13 21:16:16 +0200

    Added a class to store our intermediary form.
    The goal of this project is now:
    
    * Tokenize.
    * Convert to intermediary form.
    * Output assembly.
    
    This class is the starting point for that.

---
+++ instructions/instructions.go
@@ -0,0 +1,34 @@
+// instruction.go
+package instructions
+
+// The type of the instruction
+type InstructionType byte
+
+const (
+	// setup: push number on stack
+	Push InstructionType = 'p'
+
+	// simple operators that work with stack-numbers
+	Plus     InstructionType = '+'
+	Minus    InstructionType = '-'
+	Multiply InstructionType = '*'
+	Divide   InstructionType = '/'
+
+	// medium operators that work with stack-numbers
+	Power   InstructionType = '^'
+	Modulus InstructionType = '^'
+
+	// complex operators that work with stack-numbers
+	Sin  InstructionType = 's'
+	Cos  InstructionType = 'c'
+	Tan  InstructionType = 't'
+	Sqrt InstructionType = 'q'
+)
+
+// A single instruction will have a thing to do, and the number
+// of items to pop from the stack.
+type Instruction struct {
+	Instruction InstructionType
+	Args        int
+	Value       string
+}
