beea6758a2cc730b30bcb38962d04ec00ca9851d
Steve Kemp <steve@steve.org.uk>
2019-02-28 21:37:31 +0200

    Run the linter as part of our auto-test

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bassh
 
+# Init the modules
 go mod init
 
-# Run golang tests
+# Install the lint-tool
+go get -u golang.org/x/lint/golint
+
+# At this point failures cause aborts
+set -e
+
+# Run the linter
+golint -set_exit_status ./...
+
+# Run our golang tests
 go test ./...
 
 # Run functional test-cases
